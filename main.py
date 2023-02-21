7d865eb6deda50fa145d0188e0b529ec33a4db4b
Hegde keerthi <hegdek61@gmail.com>
2023-02-21 23:15:45 +0530

    data preparation

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+
+import matplotlib.pyplot as plt
+
+import os
+
+train_img = ImageDataGenerator(rescale=1./255,fill_mode='nearest')
+
+test_img = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_img.flow_from_directory(os.path.join("/TrainData"),target_size=(150,150),batch_size=64,class_mode='binary')
+
+test_generator = test_img.flow_from_directory(os.path.join("/TrestData"),target_size=(150,150),batch_size=64,class_mode='binary')
+
+plt.imshow(train_generator[0][0][5])
+print("Label : ",train_generator[0][1][5])
+
+plt.imshow(train_generator[0][0][60])
+print("Label : ",train_generator[0][1][60])
